0760f9d2230592bdba1246522a36d2969cb2324e
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2021-09-04 16:16:08 +0400

    RoundTo tests

---
+++ Kudiyarov.Packages.DoubleExtensions.Tests/RoundToTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Kudiyarov.Packages.DoubleExtensions.Tests
+{
+    public class RoundToTests : BaseDoubleExtensionsTest
+    {
+        [Theory]
+        [InlineData(1, 2.5, 0)]
+        [InlineData(1.25, 2.5, 2.5)]
+        [InlineData(2, 2.5, 2.5)]
+        public void ToleranceEqualZero(double value, double factor, double expected)
+        {
+            var actual = value.RoundTo(factor);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
